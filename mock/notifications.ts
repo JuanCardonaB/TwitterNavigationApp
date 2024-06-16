0000e4e68e1933b483e61c08eafa7c2dac7a098f
JuanCardonaB <juanjocarbol@gmail.com>
2024-06-15 21:38:28 -0500

    add: notifications data

--- mock/notifications.ts
+++ mock/notifications.ts
@@ -1,223 +1,101 @@
 export const notifications = [
   {
-    author: {
-      name: 'Aram Miquel',
-      screenName: 'aram_miquel',
-      avatar:
-        'https://images.unsplash.com/photo-1649011463206-cb765493f8bf?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk3NA&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480837336535646209',
-    fullText:
-      'Apple should pay more attention. It’s unfair to users, but even more to the small developers that play by the rules!',
-    retweetCount: 2,
-    replyCount: 1,
-    favoriteCount: 5,
+    id: '1480813715880615936_notification',
+    type: 'mention',
+    content: 'Gergely Orosz mentioned you in a tweet.',
+    avatar:
+      'https://images.unsplash.com/photo-1650122597661-ceccf6d50692?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk4NQ&ixlib=rb-1.2.1&q=80&w=48',
+    timestamp: '2024-06-14T10:25:16Z',
+    read: false,
   },
   {
-    author: {
-      name: 'Gergely Orosz',
-      screenName: 'GergelyOrosz',
-      avatar:
-        'https://images.unsplash.com/photo-1649827159600-237a092f95b5?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk5OQ&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480833114096214018',
-    fullText:
-      'I write about real-world hiring insights for paid subscribers of https://t.co/SLe64y6YsX - many of whom are hiring managers themselves.\n\nIt might be a hard pill to swallow, but retaining existing folks, not overloading them with too many in-person interviews comes before hiring.',
-    retweetCount: 0,
-    replyCount: 0,
-    favoriteCount: 3,
+    id: '1480824991486824448_notification',
+    type: 'retweet',
+    content: 'Toni Colom retweeted your tweet.',
+    avatar:
+      'https://images.unsplash.com/photo-1649861972512-faadc16a4571?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1NzA5Nw&ixlib=rb-1.2.1&q=80&w=48',
+    timestamp: '2024-06-14T12:06:31Z',
+    read: true,
   },
   {
-    author: {
-      name: 'ryancarson.eth',
-      screenName: 'ryancarson',
-      avatar:
-        'https://images.unsplash.com/photo-1650355255329-66a873ebab57?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk3NQ&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480806838140649472',
-    fullText:
-      'Not bullish on @LooksRareNFT and $LOOKS yet? (Disclosure: I staked my airdropped tokens. DYOR as always.)',
-    retweetCount: 2,
-    replyCount: 2,
-    favoriteCount: 13,
+    id: '1480611397503492100_notification',
+    type: 'like',
+    content: 'Amanda Emmanuel liked your tweet.',
+    avatar:
+      'https://images.unsplash.com/photo-1649894158708-bd47ad9ef4e3?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1NzA5Ng&ixlib=rb-1.2.1&q=80&w=48',
+    timestamp: '2024-06-14T07:15:02Z',
+    read: false,
   },
   {
-    author: {
-      name: 'Cris Miquel',
-      screenName: 'crismiquelg',
-      avatar:
-        'https://images.unsplash.com/photo-1650192905858-e0bc957bf390?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1NzEwNw&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480543522465976324',
-    fullText:
-      'mi único propósito para 2022 es dejar de despedirme con la mano en las videollamadas como si fuera idiota',
-    retweetCount: 2,
-    replyCount: 14,
-    favoriteCount: 64,
+    id: '1480876283382935552_notification',
+    type: 'retweet',
+    content: 'Majid Jabrayilov retweeted your tweet.',
+    avatar:
+      'https://images.unsplash.com/photo-1649080480680-221b69e21ce1?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk4Nw&ixlib=rb-1.2.1&q=80&w=48',
+    timestamp: '2024-06-14T18:12:03Z',
+    read: true,
   },
   {
-    author: {
-      name: 'Lorenzo',
-      screenName: 'Kelset',
-      avatar:
-        'https://images.unsplash.com/photo-1650346910129-e73df7b26eb0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk3Nw&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480879826781286413',
-    fullText: '🔥this is fine🔥',
-    retweetCount: 0,
-    replyCount: 0,
-    favoriteCount: 0,
+    id: '1480669856647233543_notification',
+    type: 'mention',
+    content: 'Icon Galleries mentioned you in a tweet.',
+    avatar:
+      'https://images.unsplash.com/photo-1649733484875-2e837f5b640b?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1NzA5NA&ixlib=rb-1.2.1&q=80&w=48',
+    timestamp: '2024-06-14T14:56:06Z',
+    read: false,
   },
   {
-    author: {
-      name: 'Vitto Rivabella 🥑',
-      screenName: 'VittoStack',
-      avatar:
-        'https://images.unsplash.com/photo-1649623053337-03a9aca84fc1?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1NzEwNg&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480576201878028289',
-    fullText:
-      'DAOs are the best place to become a Web3 developer.\n\nUnfortunately, only a bunch are really valuable.\n\n4 best DAOs to learn Web3  ↓',
-    retweetCount: 158,
-    replyCount: 20,
-    favoriteCount: 551,
+    id: '1480841955613581313_notification',
+    type: 'like',
+    content: 'Capture One Pro liked your tweet.',
+    avatar:
+      'https://images.unsplash.com/photo-1649393153970-fd7ec111a577?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk4OA&ixlib=rb-1.2.1&q=80&w=48',
+    timestamp: '2024-06-14T16:20:55Z',
+    read: true,
   },
   {
-    author: {
-      name: 'Justin Moore | Sponsorship Coach',
-      screenName: 'justinmooretfam',
-      avatar:
-        'https://images.unsplash.com/photo-1650213986462-1804543441c2?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk3OA&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480875135355666432',
-    fullText: 'Stop waiting for brands to contact you.',
-    retweetCount: 2,
-    replyCount: 0,
-    favoriteCount: 11,
+    id: '1480321521126019073_notification',
+    type: 'like',
+    content: 'Colin Cornaby liked your tweet.',
+    avatar:
+      'https://images.unsplash.com/photo-1651170022383-6eaa642ee2fa?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1NzA0NQ&ixlib=rb-1.2.1&q=80&w=48',
+    timestamp: '2024-06-14T02:14:12Z',
+    read: false,
   },
   {
-    author: {
-      name: 'March',
-      screenName: 'fdesbml',
-      avatar:
-        'https://images.unsplash.com/photo-1650975109944-2772703c72f7?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1NzEwNA&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480362830805311495',
-    fullText:
-      'Hand-knit jewelry by Nora Folk | She uses a mixed technique of weaving, knitting, braiding and knotting fine nylon microfilaments to create organic shapes. https://t.co/BZFBQwuHeO',
-    retweetCount: 901,
-    replyCount: 10,
-    favoriteCount: 5889,
+    id: '1480816866360766465_notification',
+    type: 'retweet',
+    content: 'Smashing Magazine retweeted your tweet.',
+    avatar:
+      'https://images.unsplash.com/photo-1649562231804-f1bfadc450d2?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1NzA5Mw&ixlib=rb-1.2.1&q=80&w=48',
+    timestamp: '2024-06-14T10:34:31Z',
+    read: true,
   },
   {
-    author: {
-      name: 'Samuel Molina',
-      screenName: 'FuKuy',
-      avatar:
-        'https://images.unsplash.com/photo-1651264042772-23891c614270?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk4MA&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480818112614260739',
-    fullText:
-      'RT @Sadface_RL: A beginners guide to health bars.\n\n#pixelart #art #animation #gamedev #indiedev #indiegamedev #indiegame https://t.co/Fhb4m…',
-    retweetCount: 177,
-    replyCount: 0,
-    favoriteCount: 0,
+    id: '1480856688060256258_notification',
+    type: 'mention',
+    content: 'Young IT Girls mentioned you in a tweet.',
+    avatar:
+      'https://images.unsplash.com/photo-1649562231804-f1bfadc450d2?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1NzA5Mw&ixlib=rb-1.2.1&q=80&w=48',
+    timestamp: '2024-06-14T17:51:28Z',
+    read: false,
   },
   {
-    author: {
-      name: 'Fumio 💡',
-      screenName: 'IK1T',
-      avatar:
-        'https://images.unsplash.com/photo-1650548211932-f6ebd1c73867?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1NzEwMw&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480811969242357760',
-    fullText:
-      "Happy #PortfolioDay !\nDear friends ❤️ I'm Fumio, a Lighting Artist 💡 currently working on Star Citizen.\nLooking forward to meeting awesome artists on twitter 😊🎨✨\n\n🖼️ https://t.co/HOEB3v9c6E\n\n#PortfolioDay #LightingArtist #gamedev #Lighting https://t.co/ROIZ4iqRkE",
-    retweetCount: 19,
-    replyCount: 1,
-    favoriteCount: 70,
+    id: '1480832043093221379_notification',
+    type: 'mention',
+    content: 'Gergely Orosz mentioned you in a tweet.',
+    avatar:
+      'https://images.unsplash.com/photo-1650122597661-ceccf6d50692?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk4NQ&ixlib=rb-1.2.1&q=80&w=48',
+    timestamp: '2024-06-14T15:20:43Z',
+    read: true,
   },
   {
-    author: {
-      name: 'ryancarson.eth',
-      screenName: 'ryancarson',
-      avatar:
-        'https://images.unsplash.com/photo-1650355255329-66a873ebab57?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk3NQ&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480809821754511361',
-    fullText: 'gm y’all!',
-    retweetCount: 0,
-    replyCount: 11,
-    favoriteCount: 19,
-  },
-  {
-    author: {
-      name: 'SonyAlphaRumors',
-      screenName: 'SonyAlphaRumors',
-      avatar:
-        'https://images.unsplash.com/photo-1650170495855-add188d60239?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk4MQ&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480859756940218370',
-    fullText:
-      'ZY Productions: This $429 Viltrox FE 24mm F1.8 Lens Is Surprisingly Good https://t.co/pAPvDYI5t4',
-    retweetCount: 0,
-    replyCount: 0,
-    favoriteCount: 2,
-  },
-  {
-    author: {
-      name: 'Sommer Panage',
-      screenName: 'Sommer',
-      avatar:
-        'https://images.unsplash.com/photo-1651236243448-ff4ea397cdf5?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1NzEwMQ&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480713374350651394',
-    fullText:
-      'I love working with people who are friendly. Who say “thank you” or cheer you on or say “good job.” These things may seem small, but they add up in the same way (but opposite direction) that all the negs and “paper cuts” do. They create a culture. They matter. Kindness matters.',
-    retweetCount: 94,
-    replyCount: 11,
-    favoriteCount: 744,
-  },
-  {
-    author: {
-      name: '@levelsio',
-      screenName: 'levelsio',
-      avatar:
-        'https://images.unsplash.com/photo-1650964858223-f4e7f5279f48?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk4Mg&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480883313787084801',
-    fullText:
-      '✨ Added a mailto: link so people can invite their friends too https://t.co/fR3ufZJeuP',
-    retweetCount: 0,
-    replyCount: 3,
-    favoriteCount: 18,
-  },
-  {
-    author: {
-      name: 'The muy ameisin Bonilista',
-      screenName: 'bonilista',
-      avatar:
-        'https://images.unsplash.com/photo-1650296231490-2deba34258bf?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1NzEwMA&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480843314349981698',
-    fullText:
-      'El patrocinio de la #Bonilista cuesta 0,036 centimos por cada persona que lee el correo. Eso, sin contar la republicación en @lavozdegalicia.\nSi te interesa reservar un patrocinio, tienes fechas disponibles a partir del 1 de mayo y más info en \nhttps://t.co/umLWuCrVnY',
-    retweetCount: 0,
-    replyCount: 0,
-    favoriteCount: 0,
-  },
-  {
-    author: {
-      name: 'John Arthorne',
-      screenName: 'jarthorne',
-      avatar:
-        'https://images.unsplash.com/photo-1651417060303-716ae896ef88?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk4NA&ixlib=rb-1.2.1&q=80&w=48',
-    },
-    id: '1480546473590857729',
-    fullText:
-      'The number of people joining Shopify RnD *today* alone is greater than the size of the RnD team when I joined 6 years ago 🤯. To my ~300 new colleagues: Welcome! 🚀',
-    retweetCount: 14,
-    replyCount: 6,
-    favoriteCount: 234,
+    id: '1480861741185650691_notification',
+    type: 'like',
+    content: 'I Am Devloper liked your tweet.',
+    avatar:
+      'https://images.unsplash.com/photo-1651354239553-0998040fa5ca?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=48&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTY1Njk5MQ&ixlib=rb-1.2.1&q=80&w=48',
+    timestamp: '2024-06-14T18:42:21Z',
+    read: false,
   },
 ];
